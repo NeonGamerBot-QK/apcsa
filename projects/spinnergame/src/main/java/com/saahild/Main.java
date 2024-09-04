6f2941a0f9d9b5a7dce38ac988fdc9f7ed574c7d
Neon <neon@saahild.com>
2024-09-04 11:45:03 -0400

    feat(project): spinner

---
+++ projects/spinnergame/src/main/java/com/saahild/Main.java
@@ -0,0 +1,13 @@
+package com.saahild;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Spinner spinner = new Spinner();
+        spinner.playRound();
+        spinner.playRound();
+        spinner.playRound();
+        spinner.playRound();
+        spinner.playRound();
+    }
+}
