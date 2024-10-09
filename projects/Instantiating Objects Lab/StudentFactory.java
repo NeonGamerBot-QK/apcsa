a761da2976c11846b8e8ad29669b837b2a905927
Neon <neon@saahild.com>
2024-10-09 13:51:11 -0400

    update(project): Student Factory

--- projects/Instantiating Objects Lab/StudentFactory.java
+++ projects/Instantiating Objects Lab/StudentFactory.java
@@ -2,15 +2,33 @@ public class StudentFactory {
 
   public static void main(String[] args) {
     // instantiate a few Student objects and call methods on them
-    Student me = new Student("Saahil", 10);
-    int myGrade = me.getGrade();
-    System.out.println(
-      "My name is " + me.getName() + " and I am in grade " + myGrade
-    );
-    me.nextGrade();
-    myGrade = me.getGrade();
-    System.out.println(
-      "My name is " + me.getName() + " and I am in grade " + myGrade
-    );
+    
+    // part1();
+    part2();
+
   }
+  public static void part1() {
+       // part 1
+       Student noNameAndNoGradeStudent = new Student();
+       System.out.println(noNameAndNoGradeStudent.getName());
+       int unkGrade = noNameAndNoGradeStudent.getGrade();
+       System.out.println(unkGrade);
+  }
+  public static void part2() {
+    // part 2
+      Student bart = new Student("Bart", 10);
+      String nameOfBart = bart.getName();
+      System.out.println(nameOfBart);
+      int gradeOfBart = bart.getGrade();
+      System.out.println(gradeOfBart);
+      // make grade for bart up to 11
+      bart.nextGrade();
+      int newGrade = bart.getGrade();
+      System.out.print(newGrade);
+      bart.setName("Im not bart");
+      String newName = bart.getName();
+      System.out.println(newName);
+
+  }
+
 }
