21e3f688b3638ce7099fa351da058a8b816712c0
Neon <neon@saahild.com>
2024-11-04 09:23:16 -0500

    docs: finish docs

--- projects/superheros/src/main/java/com/saahild/villains/Abdul.java
+++ projects/superheros/src/main/java/com/saahild/villains/Abdul.java
@@ -7,11 +7,16 @@ public class Abdul extends Person {
 
   private boolean isSacraficing = false;
   private final String name = "Abdul the Destroyer";
-
+/**
+ * Constructor for the Abdul class
+ */
   public Abdul() {
     super(70, 5);
   }
-
+/**
+ * Slash the hero
+ * @param person
+ */
   public void slashHero(Hero person) {
     person.takeDamage(getStrength());
     if (isSacraficing) {
@@ -20,18 +25,25 @@ public class Abdul extends Person {
       setStrength(getStrength() - 10);
     }
   }
-
+/**
+ * Sacrifice the next attack
+ */
   public void sacrificeNextAttack() {
     if (getHealth() < 20) return;
     isSacraficing = true;
     setHealth(getHealth() - 20);
     setStrength(getStrength() + 10);
   }
-
+/**
+ * Get the name of the villain
+ * @return
+ */
   public String getName() {
     return name;
   }
-
+/**
+ * Heal the villain
+ */
   public void heal() {
     setHealth(getHealth() + 1);
   }
--- projects/superheros/src/main/java/com/saahild/villains/Peter.java
+++ projects/superheros/src/main/java/com/saahild/villains/Peter.java
@@ -5,23 +5,34 @@ import com.saahild.Person;
 public class Peter extends Person {
 
   private final String name = "Peter the rager";
-
+/**
+ * Constructor for the Peter class
+ * @param isRaging
+ */
   public Peter(boolean isRaging) {
     super(25, 30);
     if (isRaging) {
       setStrength(getStrength() * 2);
     }
   }
-
+/**
+ * Rage
+ */
+ 
   public void lastStand() {
     setHealth(1);
     setStrength(getStrength() * 2);
   }
-
+/**
+ * +1 to strength
+ */
   public void moreStrength() {
     setStrength(getStrength() + 1);
   }
-
+/**
+ * Get the name of the villain
+ * @return
+ */
   public String getName() {
     return name;
   }
