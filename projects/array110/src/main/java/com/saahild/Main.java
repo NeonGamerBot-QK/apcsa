3dd9b2663f0c118dcdd24fa6e035ab4172af3096
Saahil <neon@saahild.com>
2025-01-10 13:52:07 -0500

    feat: todays classwork

---
+++ projects/array110/src/main/java/com/saahild/Main.java
@@ -0,0 +1,23 @@
+package com.saahild;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     Scanner   sc = new Scanner(System.in);
+     int[] arr = new int[10];
+     for(int i=0;i<10;i++){
+      System.out.println("Enter the element at index "+i);
+        arr[i] = sc.nextInt();
+     }
+     // get mean
+     double mean = 0;
+     for(int i=0;i<10;i++){
+      mean += arr[i];
+     }
+     mean = mean/10;
+     System.out.println("Mean is "+mean);
+     sc.close();
+    }
+}
